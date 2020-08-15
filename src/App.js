28378acd6a8b6e3686b57a4362652ee488b4bf7e
Jim Brothers <brothers.jim@gmail.com>
2020-08-15 19:08:20 -0400

    experiment with page routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import logo from "./logo.svg";
+
+import "./App.css";
 // import Button from 'react-bootstrap/Button'
 // this just imports bootstrap button
-import { Button, Alert } from 'react-bootstrap'
+
+import { Button, Alert } from "react-bootstrap";
 // this imports all of bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Menu from './Menu'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Menu from "./Menu";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -16,6 +27,30 @@ function App() {
       <Alert variant="danger">This is a button</Alert>
         <Button>Test Button</Button>
       </header>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <NavLink exact to="/" activeClassName="selected">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" activeClassName="selected">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home(){
+    return(
+        <div className="Home">
+            This is the homepage.
+        </div>
+    )
+}
+
+export default Home
