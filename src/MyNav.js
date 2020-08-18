240749460b718068877283d8c8d1bfc6dddae393
Jim Brothers <brothers.jim@gmail.com>
2020-08-18 19:30:38 -0400

    customized navbar to MTS

--- src/MyNav.js
+++ src/MyNav.js
@@ -8,7 +8,7 @@ function MyNav() {
   return (
     <div>
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Master-Tech Services</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
