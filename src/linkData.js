07c9bcdae5b9031541d10703079a66bd6e684b09
James Brothers <jbrothers@flvs.net>
2020-08-24 15:59:23 -0400

    Add links

--- src/linkData.js
+++ src/linkData.js
@@ -10,6 +10,21 @@ const linkData= [
         id: '77e5938f18b9a37d02657df141b7b15f',
         site: 'Master Manager',
         url: 'https://educatortools.flvs.net/educator_tools/index.html'
+    },
+    {
+        id: '558cfa417db9861a0a7fd21414e17cb2',
+        site: 'Bamboo',
+        url: 'http://app64.fvs.fvsonline.net:8085/currentActivity.action'
+    },
+    {
+        id: 'dc312932d1358f9d6014f8b195fb670f',
+        site: 'Link Manger',
+        url: 'https://l.flvsgl.com/user/plugins/multi-user/index.php'
+    },
+    {
+        id: '7848d0282b00ad2887699df313093144',
+        site: 'Course Holds',
+        url: 'https://tasks.office.com/FLVS1.onmicrosoft.com/en-US/Home/Planner/#/plantaskboard?groupId=17f0304f-76ef-4e84-a5c2-63d83cab04a1&planId=jX7XrvLw3kKnGdI8EsYVHWQAHkIN'
     }
 ]
 
