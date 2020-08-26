2b87c299bd22b1faa268584c3e115b8f64a2cc78
Jim Brothers <brothers.jim@gmail.com>
2020-08-26 19:22:07 -0400

    Updates

---
+++ precache-manifest.bd48e375da07f391422b7825296e4c8d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3fd72d4468c52db7ad5c3b119ea33da",
+    "url": "/cs-react/index.html"
+  },
+  {
+    "revision": "65f046134b370ef8130e",
+    "url": "/cs-react/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "53e629fce915c558c92f",
+    "url": "/cs-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "65f046134b370ef8130e",
+    "url": "/cs-react/static/js/2.720c2b3c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cs-react/static/js/2.720c2b3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e629fce915c558c92f",
+    "url": "/cs-react/static/js/main.e08153a5.chunk.js"
+  },
+  {
+    "revision": "6e72ecf3dc829da9f239",
+    "url": "/cs-react/static/js/runtime-main.72612c2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cs-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
